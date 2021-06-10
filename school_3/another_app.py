f365d6d2c39d7b269aa57ee0b46a1c3883d5e02f
mfarzamalam <mfarzamalam@gmail.com>
2021-06-11 00:39:02 +0500

    post_data with class based view

---
+++ school_3/another_app.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+URL = "http://127.0.0.1:8000/student/"
+
+def post_data():
+    data = {
+        'name':'Farzam',
+        'city':'krachi',
+        'age':21,
+    }
+
+    json_data = json.dumps(data)
+    r  = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+post_data()
--- school_3/api/views.py
+++ school_3/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .serializers import StudentSerializer
+import io
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
+
+@method_decorator(csrf_exempt, name='dispatch')
+class StudentApi(View):
+    def post(self, request, *args, **kwargs):
+        json_data  = request.body
+        bytes_data = io.BytesIO(json_data)
+        python_data= JSONParser().parse(bytes_data)
+        serializer = StudentSerializer(data=python_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {'msg':'Data Created Successfully'}
+            json_data= JSONRenderer().render(response)
+            return HttpResponse(json_data, content_type='application/json')
+        else:
+            json_data= JSONRenderer().render(serializer.errors)
+            return HttpResponse(json_data, content_type='application/json')
