6bc80f4dcbcb3fd6cdb5199d8ee2d324e45f935a
mfarzamalam <mfarzamalam@gmail.com>
2021-06-15 17:03:52 +0500

    Generic api with mixing

--- school_7/api/urls.py
+++ school_7/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 # from .views import hello_api
-from .views import student_api
+from .views import StudentList, StudentCreate, StudentRetrieve, StudentUpdate, StudentDelete
 
 urlpatterns = [
-    path('studentApi/', student_api.as_view()),
-    path('studentApi/<int:pk>/', student_api.as_view()),
+    path('studentApi/', StudentList.as_view()),
+    # path('studentApi/', StudentCreate.as_view()),
+    # path('studentApi/<int:pk>/', StudentRetrieve.as_view()),
+    # path('studentApi/<int:pk>/', StudentUpdate.as_view()),
+    # path('studentApi/<int:pk>/', StudentDelete.as_view()),
+    # path('studentApi/<int:pk>/', student_api.as_view()),
 ]
