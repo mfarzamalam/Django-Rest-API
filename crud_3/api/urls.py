be127068f0a874d1526ede857c5e14cad2da5036
mfarzamalam <mfarzamalam@gmail.com>
2021-06-17 02:32:44 +0500

    Generic API class view

--- crud_3/api/urls.py
+++ crud_3/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import StudentListCreate, StudentRetrieveUpdateDestroy
 
 urlpatterns = [
- 
+    path('StudentApi/', StudentListCreate.as_view()),
+    path('StudentApi/<int:pk>/', StudentRetrieveUpdateDestroy.as_view()),
 ]
