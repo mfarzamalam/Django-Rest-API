5c32c1e57b0cf087d4b7fa38b910d28474fcff3a
mfarzamalam <mfarzamalam@gmail.com>
2021-06-19 20:50:17 +0500

    init

---
+++ school_10/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework import permissions
+
+
+# Create your views here.
+class StudentApi(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
