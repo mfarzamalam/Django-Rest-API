4da398bff4252419625a97a21fb5eb53d1f2da5e
mfarzamalam <mfarzamalam@gmail.com>
2021-06-21 02:45:12 +0500

    init

---
+++ school_11/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import StudentApi
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('studentApi', StudentApi, basename='studentapi')
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
