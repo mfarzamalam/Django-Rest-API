cc57c1f170b631ccf784d847093672835ee819e9
mfarzamalam <mfarzamalam@gmail.com>
2021-06-21 03:54:12 +0500

    json web token

--- school_11/api/urls.py
+++ school_11/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from .views import StudentApi
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 router = DefaultRouter()
 router.register('studentApi', StudentApi, basename='studentapi')
@@ -8,4 +14,7 @@ router.register('studentApi', StudentApi, basename='studentapi')
 
 urlpatterns = [
     path('',include(router.urls)),
+    path('gettoken/',TokenObtainPairView.as_view()),
+    path('refreshtoken/',TokenRefreshView.as_view()),
+    path('verifytoken/',TokenVerifyView.as_view()),
 ]
--- school_11/api/views.py
+++ school_11/api/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 # Create your views here.
 class StudentApi(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
