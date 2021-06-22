220db0122763b3a138e85ed430d73b13f193de0d
mfarzamalam <mfarzamalam@gmail.com>
2021-06-22 17:17:45 +0500

    throttling in api

--- school_11/api/urls.py
+++ school_11/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import StudentApi
+from .views import StudentApi, StudentApi2
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -9,12 +9,14 @@ from rest_framework_simplejwt.views import (
 
 
 router = DefaultRouter()
-router.register('studentApi', StudentApi, basename='studentapi')
+# router.register('studentApi', StudentApi, basename='studentapi')
+router.register('studentApi', StudentApi2, basename='studentapi')
 
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('gettoken/',TokenObtainPairView.as_view()),
-    path('refreshtoken/',TokenRefreshView.as_view()),
-    path('verifytoken/',TokenVerifyView.as_view()),
+    # path('gettoken/',TokenObtainPairView.as_view()),
+    # path('refreshtoken/',TokenRefreshView.as_view()),
+    # path('verifytoken/',TokenVerifyView.as_view()),
+    path('auth/',include('rest_framework.urls')),
 ]
--- school_11/api/views.py
+++ school_11/api/views.py
@@ -3,7 +3,10 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from .throttling import CustomThrottle
 
 
 # Create your views here.
@@ -12,3 +15,13 @@ class StudentApi(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+
+
+    ### Using this api with throttling ###
+class StudentApi2(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # throttle_classes  = [AnonRateThrottle, UserRateThrottle]
+    throttle_classes  = [AnonRateThrottle, CustomThrottle]
