30ec1fdf05b429d39ad92944d6e80388b1e28bfc
mfarzamalam <mfarzamalam@gmail.com>
2021-06-22 20:20:45 +0500

    django-filter

--- school_12/api/views.py
+++ school_12/api/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework.backends import DjangoFilterBackend
 
 # Create your views here.
-class StudentApi(ListAPIView):    
+# class StudentApi(ListAPIView):    
+#     serializer_class = StudentSerializer
+#     def get_queryset(self):
+#         return Student.objects.filter(faculty_name=self.request.user)
+
+
+class StudentApi(ListAPIView): 
+    queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get_queryset(self):
-        return Student.objects.filter(faculty_name=self.request.user)
+    filter_backends  = [DjangoFilterBackend]
+    filterset_fields = ['city', 'faculty_name']
--- school_12/school_12/settings.py
+++ school_12/school_12/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'api',
 ]
 
@@ -125,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+### To user django filters globally
+# REST_FRAMEWORK = {
+#     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend']
+# }
