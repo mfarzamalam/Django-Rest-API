2888f48c9a01d3275be6b45c50d54f16baf47618
mfarzamalam <mfarzamalam@gmail.com>
2021-06-24 16:59:35 +0500

    limit offset pagination

--- school_13/api/paginations.py
+++ school_13/api/paginations.py
@@ -1,4 +1,5 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
 
 class CustomPagination(PageNumberPagination):
     page_size = 3
@@ -6,3 +7,10 @@ class CustomPagination(PageNumberPagination):
     page_size_query_param = 'record'
     max_page_size  = 7
     last_page_strings = ('last','end')
+
+
+class CustomLimitOffset(LimitOffsetPagination):
+    default_limit      = 5
+    limit_query_param  = 'mylimit'
+    offset_query_param = 'myoffset'
+    max_limit          = 6
--- school_13/api/views.py
+++ school_13/api/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from .models import School
 from .serializers import SchoolSerializer
 from rest_framework.viewsets import ModelViewSet
-from .paginations import CustomPagination
+from .paginations import CustomPagination, CustomLimitOffset
+
 
 # Create your views here.
+# class SchoolApi(ModelViewSet):
+#     queryset = School.objects.all()
+#     serializer_class = SchoolSerializer
+#     pagination_class = CustomPagination
+
+
+
 class SchoolApi(ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
-    pagination_class = CustomPagination
+    pagination_class = CustomLimitOffset
