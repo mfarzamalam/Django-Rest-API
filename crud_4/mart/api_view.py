2cbf798b2c0b39aff6bbbf3129e928dbae867ffd
mfarzamalam <mfarzamalam@gmail.com>
2021-12-15 15:55:02 +0500

    create and destroy product

--- crud_4/mart/api_view.py
+++ crud_4/mart/api_view.py
@@ -1,6 +1,8 @@
 from typing import List
+from django.core.exceptions import ValidationError
+from django.http import response
 from rest_framework import pagination
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from .serialize import ProductSerializer
 from .models import Product
 from django_filters.rest_framework import DjangoFilterBackend
@@ -9,7 +11,7 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class ProductPagination(LimitOffsetPagination):
-    default_limit = 1
+    default_limit = 2
     max_limit = 100
 
 class AllProductList(ListAPIView):
@@ -32,3 +34,34 @@ class AllProductList(ListAPIView):
 
 
         return queryset
+
+
+
+class CreateProduct(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get('price')
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({'price':'Price must be greater than zero'})
+        except ValueError:
+            raise ValidationError({'price':'A valid number is required'})
+
+        return super().create(request, *args, **kwargs)
+
+
+
+class ProductDestroy(DestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+
+    def delete(self, request, *args, **kwargs):
+        id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('product_data_{}'.format(id))
+
+        return response
--- crud_4/mart/urls.py
+++ crud_4/mart/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from mart.api_view import AllProductList
+
+
+from mart.api_view import AllProductList, CreateProduct, ProductDestroy
 
 urlpatterns = [
     path('api/v1/products/', AllProductList.as_view()),
+    path('api/v1/products/new', CreateProduct.as_view()),
+    path('api/v1/products/<int:id>/destroy', ProductDestroy.as_view()),
 ]
